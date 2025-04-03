a4c57e7205aa547b655f904b8ae41a390065a66b
planetyadv <141673051+planetyadv@users.noreply.github.com>
2025-04-03 11:17:08 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+local function clearWorkspace()
+    for _, obj in pairs(workspace:GetChildren()) do
+        -- Проверяем, не является ли объект игроком или Terrain
+        if not obj:IsA("Terrain") and not obj:IsDescendantOf(game.Players) then
+            obj:Destroy()
+        end
+    end
+end
+
+-- Запускаем очистку
+clearWorkspace()
